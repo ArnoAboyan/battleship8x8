1ec44ad65eec7c8b91ffd103288a5bf3af30f0f3
withkeygames <withkeygames@gmail.com>
2022-08-27 11:58:04 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -9,10 +9,56 @@ public class Battleship8x8 {
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+        long newShot = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000L;
+        switch (shot.charAt(0)) {
+            case 'A': break;
+            case 'B': {newShot >>>= 1; break;}
+            case 'C': {newShot >>>= 2; break;}
+            case 'D': {newShot >>>= 3; break;}
+            case 'E': {newShot >>>= 4; break;}
+            case 'F': {newShot >>>= 5; break;}
+            case 'G': {newShot >>>= 6; break;}
+            case 'H': {newShot >>>= 7; break;}
+        }
+        newShot >>>= (8 * (shot.charAt(1) - 1));
+        shots |= newShot;
+        if (ships == (ships | newShot)) {
+            return true;
+        }
+        return false;
     }
 
     public String state() {
-        throw new UnsupportedOperationException();
+
+        String strShips = Long.toBinaryString(ships);
+        String strShots = Long.toBinaryString(shots);
+        StringBuilder bufZeros = new StringBuilder();
+        if (strShots.length() < 64) {
+            for (int i = 0; i < 64 - strShots.length(); i++)
+                bufZeros.append("0");
+            strShots = bufZeros.append(strShots).toString();
+            bufZeros.delete(0, bufZeros.length());
+        }
+        if (strShips.length() < 64) {
+            for (int i = 0; i < 64 - strShips.length(); i++)
+                bufZeros.append("0");
+            strShips = bufZeros.append(strShips).toString();
+            bufZeros.delete(0, bufZeros.length());
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strShots.length(); i++) {
+            if (i%8 == 0)
+                result.append("\n");
+            if (strShips.charAt(i) == '0' && strShots.charAt(i) == '0')
+                result.append(".");
+            if (strShips.charAt(i) == '0' && strShots.charAt(i) == '1')
+                result.append("×");
+            if (strShips.charAt(i) == '1' && strShots.charAt(i) == '0')
+                result.append("☐");
+            if (strShips.charAt(i) == '1' && strShots.charAt(i) == '1')
+                result.append("☒");
+        }
+        return result.toString();
     }
+
 }
